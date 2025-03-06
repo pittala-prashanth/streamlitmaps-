12453a695fa09c3caa2874cbc9c132fb4930cfa0
Prashanth <67435164+pittala-prashanth@users.noreply.github.com>
2025-03-06 14:13:04 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,8 +5,7 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+Maps
 """
 
 st.sidebar.title("About")
@@ -15,23 +14,17 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("GIS Map")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). [GitHub repository](https://github.com/opengeos/streamlit-map-template).
     """
 )
 
 st.header("Instructions")
 
-markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
 
-"""
 
 st.markdown(markdown)
 
