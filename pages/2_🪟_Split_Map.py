10db07ca58505a7a9d2c89843ea8d09b266221d0
Prashanth <67435164+pittala-prashanth@users.noreply.github.com>
2025-03-06 14:33:19 +0530

    Update 2_🪟_Split_Map.py

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 markdown = """
 A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+<https://github.com/pittala-prashanth/streamlitmaps>
 """
 
 st.sidebar.title("About")
